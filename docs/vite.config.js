16adbd6dc90f7551bfa0a5c4e12bba8fb105cc3e
4b11c013 <jer930709@gmail.com>
2026-03-08 17:51:08 +0800

    11

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// 部署到 GitHub Pages 時使用相對路徑，避免 manifest / 靜態資源 404
 export default defineConfig({
+  base: './',
   plugins: [react()],
   publicDir: 'public',
   server: {
